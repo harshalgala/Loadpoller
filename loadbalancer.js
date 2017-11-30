bfec0f9617b56b2c9fbcde332fbcf41190fbf625
Harshal Gala <harshalbgala@gmail.com>
2017-11-30 15:28:22 -0500

    Updated LoadBalancer

--- loadbalancer.js
+++ loadbalancer.js
@@ -2,33 +2,35 @@
 install:
 1) ansible
 2) boto
-3) export creden
+3) boto3
+4) export credentials
 */
 
-var http = require('http');
+var http    = require('http');
 var request = require('request');
-var os = require('os');
+var os      = require('os');
 var express = require('express');
-var app = express();
-
-var exec = require('child_process').exec;
-
+var app     = express();
+var exec    = require('child_process').exec;
 // websocket server that website connects to.
-var io = require('socket.io')(3002);
-var allIps=[];
+var io      = require('socket.io')(4000);
 //ansible
 var Ansible = require('node-ansible');
 var ansiblePlaybookCli = require('ansible-playbook-cli-js');
+
+var allIps  = [];
+
 //
 /* TO do:
 1) Update timer
 2) Update inventory path
 3) call scaleup and scale down
-
 */
+
 //reading inventory
 var inventoryPath="/home/ubuntu/DevOps_Milestone4_Special/inventory";
 fs = require('fs')
+
 //ansible code
 var Options = ansiblePlaybookCli.Options;
 var AnsiblePlaybook = ansiblePlaybookCli.AnsiblePlaybook;
@@ -37,36 +39,25 @@ var options = new Options(
 );
 var ansiblePlaybook = new AnsiblePlaybook(options);
 
-//
-/*fs.readFile('/home/hkgurjar/Devops/DevOpsMileStone4/Monitoring/inventory_folder/inventory', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data+" *****************");
-});*/
-
 var lineReader = require('line-reader');
-
 
 lineReader.eachLine(inventoryPath, function(line, last) {
   var ip=line.substr(0,line.indexOf(' '));
   allIps.push(ip);
-  console.log(allIps);
+  //console.log(allIps);
   // do whatever you want with line...
   
 });
 
-/*setTimeout(function() {
-
-}, 5000);*/
-//console.log(allIps);
 for(var i=1;i<allIps.length;i++){
-	console.log(allIps[i]);
+    if(i == (allIps.length-1)){
+        console.log(allIps[i]);	
+    }
 }
-console.log("here..........");
+console.log("Polling the instances every 20 seconds");
 
 //requesting all instances
-var server = app.listen(3001, function () {
+var server = app.listen(4005, function () {
   var host = server.address().address;
   var port = server.address().port;
   var ansibleDir="/home/ubuntu/DevOps_Milestone4_Special/"
@@ -79,12 +70,12 @@ var server = app.listen(3001, function () {
   	}
   	
   	var myUrl="http://"+allIps[i]+":3000/health";
-  	console.log("listening: "+myUrl);
+  	console.log("Listening: "+myUrl);
 	  request(myUrl, { json: true }, ( err, res) => {
-	    	console.log('Requesting');
+	    	console.log('Requesting for the usage status');
 	    	var cpu_usage=res.body;
 	    	if(cpu_usage > 40){
-	    		console.log("Alert !!!! CPU utilization:"+cpu_usage);
+	    		console.log("!!! Alert !!!!\n Scaling Up.\n CPU utilization:"+cpu_usage);
 	    		//ansible code
 	    		ansiblePlaybook.command('-i "localhost," -c local '+ansibleDir+'scale.yaml -e "aws_secret_key='+process.env.AWS_SECRET_KEY+'" -e "aws_access_key='+process.env.AWS_ACCESS_KEY_ID+'"').then(function (data) {
 						console.log('data = ', data); 
@@ -93,13 +84,13 @@ var server = app.listen(3001, function () {
 	    	}
 	    	else if(cpu_usage<20){
 	    		//scaledown
+	    		console.log("!!!Below Threshold!!!\n Scaling down.\n CPU utilization:"+cpu_usage);
 	    		ansiblePlaybook.command('-i '+inventoryPath+' '+ansibleDir+'scale_down.yml -e "deletehostip='+allIps[i]+'"').then(function (data) {
 						console.log('data = ', data); 
 					  });
-	    		console.log("Less load. Scaling down. CPU utilization:"+cpu_usage);
 	    	}
 	    	else{
-	    		console.log("Running smoothly. CPU utilization:"+cpu_usage);
+	    		console.log("Running smoothly.\n CPU utilization:"+cpu_usage);
 	    	}
 	    	//console.log(res.body);
 	  		if (err) { 
@@ -119,6 +110,6 @@ app.use(function(req, res, next){
 	next(); 
 });
 app.get('/', function(req, res) {
-  res.send('Itrust Doctor Monkey');
+  res.send('Itrust Surgeon Monkey Running');
 });
 
