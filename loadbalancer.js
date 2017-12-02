f57aba021acd3707d58d54caa147bcae3f619326
Harshal Gala <harshalbgala@gmail.com>
2017-12-02 10:47:04 -0500

    Updated

--- loadbalancer.js
+++ loadbalancer.js
@@ -6,19 +6,19 @@ install:
 4) export credentials
 */
 
-var http    = require('http');
+var http = require('http');
 var request = require('request');
-var os      = require('os');
+var os = require('os');
 var express = require('express');
-var app     = express();
-var exec    = require('child_process').exec;
+var app = express();
+var exec = require('child_process').exec;
 // websocket server that website connects to.
-var io      = require('socket.io')(4000);
+var io = require('socket.io')(4000);
 //ansible
 var Ansible = require('node-ansible');
 var ansiblePlaybookCli = require('ansible-playbook-cli-js');
 
-var allIps  = [];
+var allIps = [];
 var allLines = [];
 //
 /* TO do:
@@ -28,98 +28,98 @@ var allLines = [];
 */
 
 //reading inventory
-var inventoryPath="/home/ubuntu/DevOps_Milestone4_Special/inventory";
+var inventoryPath = "/home/ubuntu/DevOps_Milestone4_Special/inventory";
 var fs = require('fs')
 
 //ansible code
 var Options = ansiblePlaybookCli.Options;
 var AnsiblePlaybook = ansiblePlaybookCli.AnsiblePlaybook;
 var options = new Options(
-    /* currentWorkingDirectory */ 'test'
+	/* currentWorkingDirectory */
+	'/home/ubuntu/DevOps_Milestone4_Special/'
 );
 var ansiblePlaybook = new AnsiblePlaybook(options);
 
 var lineReader = require('line-reader');
 
-lineReader.eachLine(inventoryPath, function(line, last) {
-  var ip=line.substr(0,line.indexOf(' '));
-  allIps.push(ip);
-  //console.log(allIps);
-  // do whatever you want with line...
-  
+lineReader.eachLine(inventoryPath, function (line, last) {
+	var ip = line.substr(0, line.indexOf(' '));
+	allIps.push(ip);
+	//console.log(allIps);
+	// do whatever you want with line...
+
 });
 
-for(var i=1;i<allIps.length;i++){
-    if(i == (allIps.length-1)){
-        console.log(allIps[i]);	
-    }
+for (var i = 1; i < allIps.length; i++) {
+	if (i == (allIps.length - 1)) {
+		console.log(allIps[i]);
+	}
 }
 console.log("Polling the instances every 20 seconds");
 
 //requesting all instances
 var server = app.listen(4005, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  var ansibleDir="/home/ubuntu/DevOps_Milestone4_Special/"
-  console.log('Example app listening at http://%s:%s', host, port);
+	var host = server.address().address;
+	var port = server.address().port;
+	var ansibleDir = "/home/ubuntu/DevOps_Milestone4_Special/"
+	console.log('Example app listening at http://%s:%s', host, port);
 
-  var requestLoop = setInterval(function(){
-  	for(var i=1;i<allIps.length;i++){
-  	if(allIps[i]==""){
-  		continue;
-  	}
-  	
-  	var myUrl="http://"+allIps[i]+":3000/health";
-  	console.log("Listening: "+myUrl);
-	  request(myUrl, { json: true }, ( err, res) => {
-	    	console.log('Requesting for the usage status');
-	    	var cpu_usage=res.body;
-	    	if(cpu_usage > 40){
-	    		console.log("!!! Alert !!!!\n Scaling Up.\n CPU utilization:"+cpu_usage);
-	    		//ansible code
-	    		ansiblePlaybook.command('-i "localhost," -c local '+ansibleDir+'scale.yaml -e "aws_secret_key='+process.env.AWS_SECRET_KEY+'" -e "aws_access_key='+process.env.AWS_ACCESS_KEY_ID+'"').then(function (data) {
-						console.log('data = ', data); 
-					  });
-	    		//
-	    	}
-	    	else if(cpu_usage<20){
-				//scaledown
-				var k = allIps[i]+'';
-                lineReader.eachLine(inventoryPath, function(line, last) {
-					var wholeline=line.substr(0,line.indexOf('\n'));
-					allLines.push(wholeline);
-				  });
+	var requestLoop = setInterval(function () {
+		for (var i = 1; i < allIps.length; i++) {
+			if (allIps[i] == "") {
+				continue;
+			}
 
-				  for(var j=1;j<allLines.length;j++) {
-						if(allLines[j].match(k)){
+			var myUrl = "http://" + allIps[i] + ":3000/health";
+			console.log("Listening: " + myUrl);
+			request(myUrl, {
+				json: true
+			}, (err, res) => {
+				console.log('Requesting for the usage status');
+				var cpu_usage = res.body;
+				if (cpu_usage > 40) {
+					console.log("!!! Alert !!!!\n Scaling Up.\n CPU utilization:" + cpu_usage);
+					//ansible code
+					ansiblePlaybook.command('-i "localhost," -c local ' + ansibleDir + 'scale.yaml -e "aws_secret_key=' + process.env.AWS_SECRET_KEY + '" -e "aws_access_key=' + process.env.AWS_ACCESS_KEY_ID + '"', function (err, data) {
+						console.log('data = ', data);
+					});
+					//
+				} else if ((cpu_usage < 20) && (allIps.length > 3)) {
+					//scaledown
+					var k = allIps[i] + '';
+					lineReader.eachLine(inventoryPath, function (line, last) {
+						var wholeline = line.substr(0, line.indexOf('\n'));
+						allLines.push(wholeline);
+					});
+
+					for (var j = 1; j < allLines.length; j++) {
+						if (allLines[j].match(k)) {
 							delete allLines[j];
 						}
 					}
 
-	    		console.log("!!!Below Threshold!!!\n Scaling down.\n CPU utilization:"+cpu_usage);
-	    		ansiblePlaybook.command('-i '+inventoryPath+' '+ansibleDir+'scale_down.yml -e "deletehostip='+allIps[i]+'"').then(function (data) {
-						console.log('data = ', data); 
-					  });
-	    	}
-	    	else{
-	    		console.log("Running smoothly.\n CPU utilization:"+cpu_usage);
-	    	}
-	    	//console.log(res.body);
-	  		if (err) { 
-	  			return console.log(err); 
-	  		}
-		});
-	}
-	},20000);
+					console.log("!!!Below Threshold!!!\n Scaling down.\n CPU utilization:" + cpu_usage);
+					ansiblePlaybook.command('-i ' + inventoryPath + ' ' + ansibleDir + 'scaledown.yaml -e "deletehostip=' + allIps[i] + '"', function (err, data) {
+						console.log('data = ', data);
+					});
+				} else {
+					console.log("Running smoothly.\n CPU utilization:" + cpu_usage);
+				}
+				//console.log(res.body);
+				if (err) {
+					return console.log(err);
+				}
+			});
+		}
+	}, 20000);
 
-	
 });
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
 	console.log(req.method, req.url);
 	// handle next
-	next(); 
+	next();
 });
-app.get('/', function(req, res) {
-  res.send('Itrust Surgeon Monkey Running');
+app.get('/', function (req, res) {
+	res.send('iTrust Surgeon Monkey Running');
 });
